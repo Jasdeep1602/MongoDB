031b3a9019a85fa47a66df3d2b08fb4da242565a
Jasdeep2k15 <singhjasdeep1602@gmail.com>
2024-05-23 18:50:45 +0530

    api methods with moongoose/node/express

---
+++ dbmongoose.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+app.use(express.json());
+mongoose.connect('mongodb://127.0.0.1:27017/myDatabase');
+
+const BookScheme = new mongoose.Schema({
+  title: String,
+  author: String,
+});
+
+const BookModel = mongoose.model('books', BookScheme);
+
+app.get('/books', async (req, res) => {
+  try {
+    const getBooks = await BookModel.find();
+    res.json(getBooks);
+  } catch (error) {
+    res.status(500).json('books not found');
+  }
+});
+
+app.post('/books', async (req, res) => {
+  try {
+    const { title, author } = req.body;
+    const newBook = new BookModel({ title, author });
+    await newBook.save();
+    res.status(201).json(newBook);
+  } catch (error) {
+    res.status(500).json({ error: ' not added' });
+  }
+});
+
+app.put('/books/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, author } = req.body;
+    const updatedBook = await BookModel.findByIdAndUpdate(id, {
+      title,
+      author,
+    });
+    res.json(updatedBook);
+  } catch (error) {
+    res.status(500).json({ error: ' not added' });
+  }
+});
+
+app.delete('/books/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    await BookModel.findByIdAndDelete(id);
+    res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ error: ' not added' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('connected');
+});
